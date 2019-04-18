66ec4495df426f422a872d875ee230a1299fae58
Tarun Dhiman <tarun.dhiman@huntoffices.com>
2019-04-18 17:08:04 +0530

    adedd first api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """ Test Api View. """
+
+    def get(self, request, format=None):
+        """ Returns a list of ApiView features """
+
+        an_apiview = [
+            'uses HTTP methods as function (get,post,patch,put,delete)',
+            'it is similiar to a traditional Django view',
+            'Is mapped manually to URLs'
+        ]
+
+        return Response({
+            'message':'Hello !',
+            'an_apiview': an_apiview
+        })
